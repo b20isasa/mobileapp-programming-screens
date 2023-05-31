35a02751ac1d268d5a504efa5fa0699d27983275
b20isasa <b20isasa@student.his.se>
2023-05-31 14:24:21 +0200

    Lagt till en till en knapp

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -10,11 +10,12 @@ import android.content.Intent;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        button = findViewById(R.id.button);
     }
 }
